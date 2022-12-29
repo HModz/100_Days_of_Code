97ec7eefe69e139595f7d85e9e3a891b8813c17b
HModz <65021738+HModz@users.noreply.github.com>
2022-12-29 09:38:55 +0100

    Final

--- Day18/Colorgram/paint.py
+++ Day18/Colorgram/paint.py
@@ -1,4 +1,4 @@
-import colorgram
+"""import colorgram
 
 colors_list=[]
 number_of_colors = 6
@@ -11,4 +11,32 @@ for i in range(number_of_colors):
     print(rgb)
     colors_list.append(color)
 
-print(colors_list)
+print(colors_list)"""
+
+from turtle import Turtle, Screen
+import random
+
+t = Turtle()
+screen = Screen()
+screen.colormode(255)
+
+colors_list = [(230, 228, 224),
+               (229, 223, 226),
+               (217, 226, 220),
+               (195, 172, 121),
+               (221, 226, 231),
+               (156, 97, 59)]
+a=20
+
+def row():
+    for i in range(9):
+        t.dot(5,random.choice(colors_list))
+        t.up()
+        t.fd(20)
+for i in range(9):
+    row()
+    t.setpos(0,a)
+    a+=20
+t.ht() 
+
+screen.mainloop
