4e0e101c25813700f284adb457b0dac00cabe6ba
HModz <65021738+HModz@users.noreply.github.com>
2023-01-04 19:47:27 +0100

    Day 25 Final
    Squirell Counter, US States Game

---
+++ Day25/main.py
@@ -0,0 +1,14 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+grey = data[data["Primary Fur Color"] == "Gray"]
+cinnamon = data[data["Primary Fur Color"] == "Cinnamon"]
+black = data[data["Primary Fur Color"] == "Black"]
+
+data_dict = {"Fur Color":["Gray", "Cinnamon", "Black"],
+        "Count":[len(grey), len(cinnamon), len(black)]
+        }
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("Count")
